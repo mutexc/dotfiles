0defa7f6b18a81c42e708c6a3acdfbba586df3d9
mutexc <abdrahimwnzr@gmail.com>
2024-10-12 16:12:06 +0100

    added treesitter and setup go-lang lsp to work

--- nvim/init.lua
+++ nvim/init.lua
@@ -13,6 +13,13 @@ vim.opt.rtp:prepend(lazypath)
 
 require("lazy").setup({
 	{
+	    'windwp/nvim-autopairs',
+	    event = "InsertEnter",
+	    config = true
+	    -- use opts = {} for passing setup options
+	    -- this is equivalent to setup({}) function
+	},
+	{
 		"sainnhe/gruvbox-material",
 		priority = 1000,
 	},
@@ -46,6 +53,16 @@ require("lazy").setup({
 	-- Mason setup
 	"williamboman/mason-lspconfig.nvim",
 	"nvim-tree/nvim-tree.lua",
+	-- treesitter stuff
+	{
+		-- Highlight, edit, and navigate code
+		'nvim-treesitter/nvim-treesitter',
+		dependencies = {
+		  'nvim-treesitter/nvim-treesitter-textobjects',
+		},
+		build = ':TSUpdate',
+	},
+
 })
 
 -- completion plugin setup
@@ -96,6 +113,7 @@ require("mason-lspconfig").setup({
 
 local lspconfig = require("lspconfig")
 lspconfig.rust_analyzer.setup({})
+lspconfig.gopls.setup({})
 lspconfig.zls.setup({})
 
 -- mappings for diagnostics
@@ -151,6 +169,81 @@ local keymap = vim.keymap.set
 -- keymappings to center searching to the center of the buffer
 keymap("n", "n", "nzz", mappings)
 keymap("n", "N", "Nzz", mappings)
+
+
+-- configure treesitter
+-- Defer Treesitter setup after first render to improve startup time of 'nvim {filename}'
+vim.defer_fn(function()
+  require('nvim-treesitter.configs').setup {
+    -- Add languages to be installed here that you want installed for treesitter
+    ensure_installed = { 'c', 'cpp', 'go', 'lua','rust', 'tsx', 'javascript', 'typescript', 'vimdoc', 'vim', 'bash' },
+
+    -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
+    auto_install = false,
+    -- Install languages synchronously (only applied to `ensure_installed`)
+    sync_install = false,
+    -- List of parsers to ignore installing
+    ignore_install = {},
+    -- You can specify additional Treesitter modules here: -- For example: -- playground = {--enable = true,-- },
+    modules = {},
+    highlight = { enable = true },
+    indent = { enable = true },
+    incremental_selection = {
+      enable = true,
+      keymaps = {
+        init_selection = '<c-space>',
+        node_incremental = '<c-space>',
+        scope_incremental = '<c-s>',
+        node_decremental = '<M-space>',
+      },
+    },
+    textobjects = {
+      select = {
+        enable = true,
+        lookahead = true, -- Automatically jump forward to textobj, similar to targets.vim
+        keymaps = {
+          -- You can use the capture groups defined in textobjects.scm
+          ['aa'] = '@parameter.outer',
+          ['ia'] = '@parameter.inner',
+          ['af'] = '@function.outer',
+          ['if'] = '@function.inner',
+          ['ac'] = '@class.outer',
+          ['ic'] = '@class.inner',
+        },
+      },
+      move = {
+        enable = true,
+        set_jumps = true, -- whether to set jumps in the jumplist
+        goto_next_start = {
+          [']m'] = '@function.outer',
+          [']]'] = '@class.outer',
+        },
+        goto_next_end = {
+          [']M'] = '@function.outer',
+          [']['] = '@class.outer',
+        },
+        goto_previous_start = {
+          ['[m'] = '@function.outer',
+          ['[['] = '@class.outer',
+        },
+        goto_previous_end = {
+          ['[M'] = '@function.outer',
+          ['[]'] = '@class.outer',
+        },gruvbox_dark
+      },
+      swap = {
+        enable = true,
+        swap_next = {
+          ['<leader>a'] = '@parameter.inner',
+        },
+        swap_previous = {
+          ['<leader>A'] = '@parameter.inner',
+        },
+      },
+    },
+  }
+end, 0)
+
 
 set.clipboard = "unnamedplus"
 set.showmode = false
