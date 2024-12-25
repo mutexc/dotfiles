4d6340274ca1b65b6f5d2389df1c9fe7fe7069dc
mutexc <abdrahimwnzr@gmail.com>
2024-12-25 16:59:22 +0100

    changes

--- install_config.sh
+++ install_config.sh
@@ -13,7 +13,7 @@ exec ln -s /home/op/.config/dotfiles/alacritty/.alacritty.toml /home/op &
 exec ln -s /home/op/.config/dotfiles/nvim /home/op/.config &
 exec ln -s /home/op/.config/dotfiles/fuzzel /home/op/.config &
 exec ln -s /home/op/.config/dotfiles/fish /home/op/.config &
-exec ln -s /home/op/.config/dotfiles/sway /home/op/.config &
+exec ln -s /home/op/.config/dotfiles/hypr /home/op/.config &
 exec ln -s /home/op/.config/dotfiles/waybar /home/op/.config &
 exec ln -s /home/op/.config/dotfiles/zed /home/op/.config &
 exec ln -s /home/op/.config/dotfiles/.themes /home/op/ &
