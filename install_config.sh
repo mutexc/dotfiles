c4b2563136199246acbb8507b380cd5d2287c275
mutexc <abdrahimwnzr@gmail.com>
2025-01-03 09:43:33 +0100

    changes and added new vim config

--- install_config.sh
+++ install_config.sh
@@ -1,4 +1,3 @@
-exec rm -rf /home/op/.config/zed &
 exec rm -rf /home/op/.themes &
 exec rm -rf /home/op/.config/fish &
 exec rm -rf /home/op/.config/alacritty &
@@ -6,12 +5,13 @@ exec rm -rf /home/op/.config/sway &
 exec rm -rf /home/op/.alacritty.toml &
 exec rm -rf /home/op/.tmux.conf &
 exec rm -rf /home/op/.config/nvim &
+exec rm -rf /home/op/.vimrc &
 exec rm -rf /home/op/.config/fuzzel &
 exec ln -s /home/op/.config/dotfiles/alacritty/.alacritty.toml /home/op &
-exec ln -s /home/op/.config/dotfiles/nvim /home/op/.config &
+exec ln -s /home/op/.config/dotfiles/vim/.vimrc /home/op/.vimrc &
 exec ln -s /home/op/.config/dotfiles/fuzzel /home/op/.config &
 exec ln -s /home/op/.config/dotfiles/fish /home/op/.config &
 exec ln -s /home/op/.config/dotfiles/sway /home/op/.config &
-exec ln -s /home/op/.config/dotfiles/zed /home/op/.config &
 exec ln -s /home/op/.config/dotfiles/.themes /home/op/ &
 exec ln -s /home/op/.config/dotfiles/tmux/.tmux.conf /home/op/ &
+exec ln -s home/op/.config/dotfiles/nvim /home/op/.config &
---
+++ vim/.vimrc
@@ -0,0 +1,28 @@
+set number
+set noswapfile
+set shiftwidth=4
+set smarttab
+set expandtab
+set nobackup
+set scrolloff=7
+set nowrap
+set incsearch
+set ignorecase
+set smartcase
+set showcmd
+set showmatch
+set hlsearch
+set history=1000
+set cursorline
+set mouse=a
+set relativenumber
+set clipboard="unnamedplus"
+colorscheme torte
+syntax on
+
+let mapleader = " "
+call plug#begin()
+    Plug 'junegunn/fzf', { 'do': { -> fzf#install() } }
+    Plug 'junegunn/fzf.vim'
+call plug#end()
+nnoremap <Leader>f :FZF<CR>
