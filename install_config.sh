166d65c331d306e35599d7df45b28e7e3e90ffbe
mutexc <abdrahimwnzr@gmail.com>
2025-01-03 10:05:46 +0100

    changes and added new vim config

--- install_config.sh
+++ install_config.sh
@@ -5,6 +5,7 @@ exec rm -rf /home/op/.config/sway &
 exec rm -rf /home/op/.alacritty.toml &
 exec rm -rf /home/op/.tmux.conf &
 exec rm -rf /home/op/.config/nvim &
+exec rm -rf /home/op/.config/foot &
 exec rm -rf /home/op/.vimrc &
 exec rm -rf /home/op/.config/fuzzel &
 exec ln -s /home/op/.config/dotfiles/alacritty/.alacritty.toml /home/op &
@@ -14,4 +15,5 @@ exec ln -s /home/op/.config/dotfiles/fish /home/op/.config &
 exec ln -s /home/op/.config/dotfiles/sway /home/op/.config &
 exec ln -s /home/op/.config/dotfiles/.themes /home/op/ &
 exec ln -s /home/op/.config/dotfiles/tmux/.tmux.conf /home/op/ &
-exec ln -s home/op/.config/dotfiles/nvim /home/op/.config &
+exec ln -s /home/op/.config/dotfiles/nvim /home/op/.config &
+exec ln -s /home/op/.config/dotfiles/foot /home/op/.config &
