eda69f6285216dd16027342af9ad1d32f8067268
mutexc <abdrahimwnzr@gmail.com>
2025-04-28 11:15:31 +0100

    stuff

--- fish/config.fish
+++ fish/config.fish
@@ -1,8 +1,9 @@
 # for a bitsavvy in rust
-set -Ux DATABASE_URL_C "postgresql://op:aobd1202@localhost:5432/abs_db"
-set -Ux DATABASE_URL "postgresql://op:aobd1202@localhost:5432/abs_db"
+set -x DATABASE_URL "postgresql://op:aobd1202@localhost:5432/trafriqa?sslmode=disable"
+# set -Ux DATABASE_URL_C "postgresql://op:aobd1202@localhost:5432/abs_db"
 # set -Ux DATABASE_URL "postgresql://op:aobd1202@localhost/abs_db"
 set -Ux ASSETS_FOLDER /home/op/abs/src/assets
+set LANG C.UTF-8
 
 # for a bitsavvy rewrite in go
 # set -Ux GO_DATABASE_URL_C "host=localhost port=5999 user=op password=aobd1202 dbname=abs_db sslmode=disable"
