efa13fde2b7b02f212b67447651600b469f63ad0
mutexc <abdrahimwnzr@gmail.com>
2025-05-08 15:27:03 +0100

    added screenshots

--- nvim/init.lua
+++ nvim/init.lua
@@ -39,6 +39,7 @@ vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 
 -- Diagnostic keymaps
 vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostic [Q]uickfix list' })
+vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open diagnostic [Q]uickfix list' })
 
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
