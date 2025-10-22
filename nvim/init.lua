ccf093f857e18414d9ad25c684aa9371a6eaafa5
mutexc <abdrahimwnzr@gmail.com>
2025-10-22 21:43:47 +0100

    reduced config, removed bloat

--- nvim/init.lua
+++ nvim/init.lua
@@ -163,16 +163,12 @@ require('lazy').setup {
                                 callSnippet = 'Replace',
                             },
                             diagnostics = { globals = { 'vim' }, disable = { 'missing-fields' } },
-                            format = { enable = false },
                         },
                     },
                 },
             }
 
             local ensure_installed = vim.tbl_keys(servers or {})
-            vim.list_extend(ensure_installed, {
-                'stylua', -- Used to format Lua code
-            })
             require('mason-tool-installer').setup { ensure_installed = ensure_installed }
             require('mason-lspconfig').setup {
                 ensure_installed = {},
@@ -278,46 +274,6 @@ require('lazy').setup {
                 additional_vim_regex_highlighting = { 'ruby' },
             },
             indent = { enable = true, disable = { 'ruby' } },
-        },
-    },
-    {
-        'stevearc/conform.nvim',
-        event = { 'BufWritePre' },
-        cmd = { 'ConformInfo' },
-        keys = {
-            {
-                '<leader>f',
-                function()
-                    require('conform').format { async = true, lsp_format = 'fallback' }
-                end,
-                mode = '',
-                desc = '[F]ormat buffer',
-            },
-        },
-        opts = {
-            notify_on_error = false,
-            format_on_save = function(bufnr)
-                -- Disable "format_on_save lsp_fallback" for languages that don't
-                -- have a well standardized coding style. You can add additional
-                -- languages here or re-enable it for the disabled ones.
-                local disable_filetypes = { c = true, cpp = true }
-                if disable_filetypes[vim.bo[bufnr].filetype] then
-                    return nil
-                else
-                    return {
-                        timeout_ms = 500,
-                        lsp_format = 'fallback',
-                    }
-                end
-            end,
-            formatters_by_ft = {
-                lua = { 'stylua' },
-            },
-            formatters = {
-                stylua = {
-                    prepend_args = { '--indent-type', 'Spaces', '--indent-width', '4' },
-                },
-            },
         },
     },
 }
